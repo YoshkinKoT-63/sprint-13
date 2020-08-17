97b65901cf89a7947d33edce10f76bfa86315b46
YoshkinKoT-63 <yoshkinkot63@gmail.com>
2020-08-17 21:40:45 +0400

    added project structure

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const cards = require('./routes/cards.js');
 const users = require('./routes/users.js');
@@ -6,6 +7,13 @@ const users = require('./routes/users.js');
 const app = express();
 
 const { PORT = 3000 } = process.env;
+
+// подключаемся к серверу mongo
+mongoose.connect('mongodb://localhost:27017/mydb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
